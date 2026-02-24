d1cc07b58e4e081cb113fa4c03df13a62b932e76
moin <khanmoin4947@gmail.com>
2026-02-24 10:40:13 +0530

    Moving to backend
    the frontend is almost done now initialsiling the backend

---
+++ Server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
